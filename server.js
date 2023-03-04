e2bbfd6d7f152868bbeecb27bb043d6bc095842e
barakaganai <barakaganai01@gmail.com>
2023-03-04 14:59:23 +0300

    import authRoute in server.js

--- server.js
+++ server.js
@@ -5,19 +5,24 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users.js");
+const authRoute = require("./routes/auth.js");
 
 // dotenv configuration
 dotenv.config();
 
 // mongoose connection
-mongoose.connect(process.env.MONGO_URL, () => {
-  console.log("connected to mongodb");
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("mongoDB connected successfully"))
+  .catch((err) => console.error("mongoDB fail to connect", err));
 
 // middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(5000, () => {
   console.log("The backend is listening and here we go!.");
