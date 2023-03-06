03d4e4fa00fa2d6dd01b95b2bbdb4566f2b43399
barakaganai <barakaganai01@gmail.com>
2023-03-06 17:53:32 +0300

    create route to unFollow a user

--- routes/users.js
+++ routes/users.js
@@ -72,4 +72,25 @@ router.put("/:id/follow", async (req, res) => {
   }
 });
 
+//unFollow a user
+router.put("/:id/unFollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("you unFollowed this user");
+      } else {
+        res.status(403).json("you already unFollowed this user");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You can't unFollow yourself");
+  }
+});
+
 module.exports = router;
