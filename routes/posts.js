1f55e1f7c163a451591e6305f38c7f9b0896f96d
barakaganai <barakaganai01@gmail.com>
2023-03-07 16:20:58 +0300

    create like or dislike route

--- routes/posts.js
+++ routes/posts.js
@@ -42,4 +42,20 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+//like or dislike a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("You liked this post");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("You disliked this post");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
